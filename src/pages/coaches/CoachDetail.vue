36b52212840a140af2505433c0b2f6bd5c57b90a
sanderdsz <sanderdsz@gmail.com>
2021-06-24 14:00:45 -0300

    added vuex and dummy data

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  hello world
+  <router-view></router-view>
+  <router-link to="/coaches/1/contact">Contact</router-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
