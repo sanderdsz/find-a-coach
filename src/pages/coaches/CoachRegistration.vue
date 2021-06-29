b9606da0c1dfcfec3f798ea3c9106a0e5aa43208
sanderdsz <sanderdsz@gmail.com>
2021-06-28 22:11:15 -0300

    form register done

---
+++ src/pages/coaches/CoachRegistration.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach.</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: { CoachForm },
+};
+</script>
